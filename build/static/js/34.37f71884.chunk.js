67fd6c59aba56a526bf8f4adf6043dfaf4feeb5d
arjun <arjun@method-hub.com>
2021-06-22 20:44:41 +0530

    update build

--- build/static/js/34.37f71884.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[34],{697:function(e,t,c){"use strict"},911:function(e,t,c){"use strict";c.r(t);c(1);var r=c(629),i=(c(626),c(697),c(17));t.default=function(e){e.match;return Object(i.jsx)(r.I,{children:"add users    "})}}}]);
-//# sourceMappingURL=34.37f71884.chunk.js.map
